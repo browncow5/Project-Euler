5ce2351106704fefb7c7dad7dd69d27c6ac3c0bb
browncow5 <ryan.donat@colorado.edu>
2018-12-14 10:29:37 -0700

    Problem 1 and RunProblems copied from another local project.

---
+++ problems/RunProblems.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 2, 2017
+
+@author: ryand
+'''
+'''
+Github ready
+'''
+import problems
+from problems.Problem1 import Problem1
+
+if __name__ == '__main__':
+    print("Main method of RunProblems")
+
+    prob1 = Problem1()
+    prob1.run()
+    
